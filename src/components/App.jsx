6a250ba3dbeb6dd5f8a236f627b1b33b64261587
DanStelmakh <danylo.stelmakh@gmail.com>
2022-12-13 21:59:18 +0200

    redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import React from 'react';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 // import shortid from 'shortid';
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import { Input } from 'components/ContactForm/ContactForm.styled';
-import { FilterTitle } from 'components/Filter/Filter.styled';
-import { useDispatch, useSelector } from 'react-redux';
-import { filteredContacts } from 'redux/actions';
-import { useState } from 'react';
+// import React from 'react';
+// import { Input } from 'components/ContactForm/ContactForm.styled';
+// import { FilterTitle } from 'components/Filter/Filter.styled';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { filteredContacts } from 'redux/actions';
+// import { useState } from 'react';
 
-export const Filter = () => {
-  const filterContact = useSelector(state => state.filter);
-  const dispatch = useDispatch();
+// export const Filter = () => {
+//   const filterContact = useSelector(state => state.filter);
+//   const dispatch = useDispatch();
 
-  const changeFilter = e => {
-    const data = e.currentTarget.value;
-    return data;
-  };
-  return (
-    <label>
-      <FilterTitle>Find contacts by name:</FilterTitle>
-      <Input
-        type="text"
-        name="filter"
-        placeholder="Enter name"
-        //   value={}
-        //   onChange={}
-      ></Input>
-    </label>
-  );
-};
+//   const changeFilter = e => {
+//     const data = e.currentTarget.value;
+//     return data;
+//   };
+//   return (
+//     <label>
+//       <FilterTitle>Find contacts by name:</FilterTitle>
+//       <Input
+//         type="text"
+//         name="filter"
+//         placeholder="Enter name"
+//         //   value={}
+//         //   onChange={}
+//       ></Input>
+//     </label>
+//   );
+// };
